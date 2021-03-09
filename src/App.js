8b1648da9d11ed9df0cc44a16fea7fd22221b75b
theking465 <9497296+theking465@users.noreply.github.com>
2021-03-09 22:09:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const snowflake = require('discord-snowflake');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      inputValue:""
+    }
+    this.handleIdChange = this.handleIdChange.bind(this);
+    this.isValidSnowflake = this.isValidSnowflake.bind(this);
+    this.snowflakeDate = this.snowflakeDate.bind(this);
+  }
+  //let snowflakeDate = new Date(0);
+  handleIdChange(event){
+    console.log(event.target.value)
+    this.setState({inputValue : event.target.value});
+  }
+
+  isValidSnowflake(id){
+    if(id === "") return false;
+    return id.match(/^[0-9]*$/) && !isNaN(this.snowflakeDate());
+  }
+  snowflakeDate(){
+    return snowflake(this.state.inputValue);
+  }
+
+    render() {
+      return(
+        <div className = "root" >
+          <h1 className="title">Discord snowflake</h1>
+          <input className="input" value={this.state.inputValue} onChange={this.handleIdChange}></input>
+          <div className="date">
+
+          {this.isValidSnowflake(this.state.inputValue) ?
+            <div className="valid">
+              {this.snowflakeDate().toLocaleString()}
+            </div>
+            :
+            <div className="invalid">
+              invalid snowflake
+            </div>
+          }
+
+          </div>
+      </div>
+      );
+    }
 }
 
 export default App;
